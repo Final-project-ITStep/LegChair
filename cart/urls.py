0c942b70b49fd1786dac3d229cf03a56260006ea
MaxDavidchuk <dp_maxim@outlook.com>
2022-12-14 07:50:27 +0200

    2022-12-14

--- cart/urls.py
+++ cart/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('ajax_cart', ajax_cart),
 	path('ajax_cart_display', ajax_cart_display),
 	path('ajax_cart_delete', ajax_cart_delete),
+    path('ajax_wish_update', ajax_wish_update),
     path('', index),
     path('index', index),
     path('wish', wish),
--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import *
 
 
 urlpatterns = [
     path('', index),
     path('index', index),
+    re_path(r'^detail/(?P<item>[0-9]+)$', detail),
 ]
